114d5873af27360b2a0a2cefa0482ac902a65e14
narakami <nazar-krasovskiy@mail.ru>
2024-06-06 19:36:06 +0300

    сделан laravel

---
+++ app/Http/Controllers/Auth/registercontroller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class registercontroller extends Controller
+{
+    public function viev()
+    {
+
+    }
+    public function store()
+    {
+        
+    }
+}
---
+++ resources/views/registration.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.scss">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="back"></div>
+    <div class="container">
+        <div class="register">
+            <div class="card">
+                <div class="card-head-r">
+                    <img src="img/Group 30.png" alt="">
+                </div>
+                <div class="card-body">
+                    <p class="Peapods">Peapods</p>
+                    <div class="auth">
+                        <p class="reg link-primary ath">авторизация</p>
+                        <form action="">
+                            <input class="forma" type="text" name="" id="" placeholder="name">
+                            <input class="forma" type="text" name="" id="" placeholder="password">
+                            <input type="submit" class="suba">
+                        </form>
+                    </div>
+                    <div class="dropdowna flex">
+                        <div class="dropup-center dropup">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              регистрация
+                            </button>
+                            <ul class="dropdown-menu">
+                              <form action="">
+                              <li class="sub"><input class="dropdown-item" type="text" name="" id="" placeholder="name"></li>
+                              <li><input class="dropdown-item" type="text" name="" id="" placeholder="password"></li>
+                              <li><input type="submit" class="suba"></li>
+                              </form>
+                            </ul>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</html>
